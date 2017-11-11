19bbf3060d080205ea8cc7966ad8c3b17da55e11
PJ Trainor <trainorpj@gmail.com>
2017-11-11 10:20:24 -0500

    update build

--- build/index.js
+++ build/index.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 var compareToBaseline = function compareToBaseline(baseline, otherObject, field, compareFcn) {
-  return compareFcn(baseline[(field, otherObject[field])]);
+  return compareFcn(baseline[field], otherObject[field]);
 };
 
 exports.default = compareToBaseline;
